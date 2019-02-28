9009fd2a8934c58f733f8409ab83ad732e4f299c
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2019-02-28 17:08:53 +0100

    switch to UDP multicast

--- discover.py
+++ discover.py
@@ -1,12 +1,16 @@
-from socket import *
+import socket
 
-ss = socket(AF_INET, SOCK_DGRAM) 
-ss.bind(('0.0.0.0', 45454))
+REPLY_PORT = 45454
+MULTICAST_GROUP = '224.0.21.132'
+MULTICAST_PORT = 54545
+MULTICAST_TTL = 2
 
-cs = socket(AF_INET, SOCK_DGRAM)  
-cs.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)  
-cs.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) 
-cs.sendto(gethostname().encode('UTF-8'), ('255.255.255.255', 54545))
+ss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+ss.bind(('0.0.0.0', REPLY_PORT))
+
+cs = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) 
+cs.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+cs.sendto(socket.gethostname().encode('UTF-8'), (MULTICAST_GROUP, MULTICAST_PORT))
 cs.close()
 
 while True:
--- server_discover.py
+++ server_discover.py
@@ -1,9 +1,23 @@
-from socket import *
+import socket
+import struct
 
-ss = socket(AF_INET, SOCK_DGRAM) 
-ss.bind(('0.0.0.0', 54545))
+REPLY_PORT = 45454
+MULTICAST_GROUP = '224.0.21.132'
+MULTICAST_PORT = 54545
+MULTICAST_TTL = 2
+
+ss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+try:
+    ss.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+except AttributeError:
+    pass
+ss.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL) 
+ss.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
+ss.bind(('', MULTICAST_PORT))
+MEMBERSHIP = struct.pack("4sl", socket.inet_aton(MULTICAST_GROUP), socket.INADDR_ANY)
+ss.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, MEMBERSHIP)
 
 while True:
     (bytes, address) = ss.recvfrom(1024)
     print(f'{address[0]}: {bytes}')
-    ss.sendto(gethostname().encode('UTF-8'), (address[0], 45454))
+    ss.sendto(socket.gethostname().encode('UTF-8'), (address[0], REPLY_PORT))
